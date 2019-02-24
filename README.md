e9c433f8256078bf71bfd579252b8378f38e5db1
jookimmy <30672093+jookimmy@users.noreply.github.com>
2019-02-23 19:53:47 -0600

    pivoted to p2p on other platform for demo

--- README.md
+++
@@ -1,2 +0,0 @@
-# optumRx-patient-app
-Personalized mobile app for patients of Optum Rx to be able to treat themselves
--- index.js
+++ index.js
@@ -1,10 +1,64 @@
-/**
- * @format
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+// var Peer = require('simple-peer');
+// var peer1 = new Peer({
+// 	initiator: location.hash == '#init',
+// 	trickle: false
+// })
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// peer.on('signal', function(data) {
+// 	document.getElementByID('yourID').value = JSON.stringify(data)
+// })
+
+// document.getElementByID('connect').addEventListener('click', function() {
+// 	var otherID = JSON.parse(document.getElementByID('otherID').value)
+// 	peer.signal(otherID)
+// })
+
+// document.getElementByID('send').addEventListener('click', function() {
+// 	var yourMessage = document.getElementByID('yourMessage').value
+// 	peer.send(yourMessage)
+// })
+
+// peer.on('data', function (data) {
+// 	document.getElementByID('messages').textContet += data + '\n'
+// })
+var getUserMedia = require('getusermedia');
+
+getUserMedia({ video: true, audio: true }, function (err, stream) {
+  if (err) return console.error(err);
+
+  var Peer = require('simple-peer');
+  
+  var patient = new Peer({
+    initiator: location.hash === '#patient',
+    trickle: false,
+    stream: stream
+  });
+
+
+  patient.on('signal', function (data) {
+    document.getElementById('yourId').value = JSON.stringify(data)
+  });
+
+  document.getElementById('connect').addEventListener('click', function () {
+    var otherId = JSON.parse(document.getElementById('otherId').value);
+    patient.signal(otherId);
+  });
+
+  document.getElementById('send').addEventListener('click', function () {
+    var yourMessage = document.getElementById('yourMessage').value;
+    patient.send(yourMessage);
+  });
+
+  patient.on('data', function (data) {
+    document.getElementById('messages').textContent += data + '\n';
+  });
+
+  patient.on('stream', function (stream) {
+    var video = document.createElement('video');
+    document.body.appendChild(video);
+
+    video.srcObject = stream;
+    video.play();
+  });
+});
